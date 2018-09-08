a298957f362c1b01aa93eef107b72f225e9239f8
Amanda <missamandawong@gmail.com>
2018-09-07 22:36:45 -0700

    Import InputPanel and ToDoList component to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import InputPanel from './InputPanel/input-panel';
+import ToDoList from './ToDoList/to-do-list';
 import './App.css';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div id="App">
         <InputPanel />
+        <ToDoList />
       </div>
     );
   }
