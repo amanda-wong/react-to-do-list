58e6d7cc757cdc2aae3d26e1776e03c806a554bc
Amanda <missamandawong@gmail.com>
2018-09-08 00:22:12 -0700

    Pass onClick and onChange handlers to Input panel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,52 @@
 import React, { Component } from 'react';
 import InputPanel from './InputPanel/input-panel';
 import ToDoList from './ToDoList/to-do-list';
+import ToDoItem from './ToDoItem/to-do-item';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div id="App">
-        <InputPanel />
-        <ToDoList />
-      </div>
-    );
-  }
+    constructor() {
+        super();
+        this.state = {
+            heading: '',
+            description: '',
+            submitToDoItem: false
+        }
+    }
+
+
+    headingChangeHandler = (event) => {
+        this.setState({
+            heading: event.target.value
+        })
+    }
+
+    descriptionChangeHandler = (event) => {
+        this.setState({
+            description: event.target.value
+        })
+    }
+
+    submitToDoHandler = () => {
+        this.setState({
+            submitToDoItem: true 
+        })
+    }
+
+    render() {
+        return (
+            <div id="App">
+                <InputPanel 
+                    submitHandler={this.submitToDoHandler} 
+                    headingChange={this.headingChangeHandler}
+                    descriptionChange={this.descriptionChangeHandler} />
+                <ToDoList 
+                    description={this.state.description}
+                    heading={this.state.heading}
+                    createToDo={this.state.createToDo} />
+            </div>
+        );
+    }
 }
 
 export default App;
