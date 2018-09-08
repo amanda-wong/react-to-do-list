a05631f76e23686c4ab3643118dcaeb4f933dffc
Amanda <missamandawong@gmail.com>
2018-09-08 01:19:55 -0700

    Add logic to display to do item

--- src/ToDoList/to-do-list.js
+++ src/ToDoList/to-do-list.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import ToDoItem from './../ToDoItem/to-do-item';
 import './to-do-list.css';
 
 class ToDoList extends Component {
     render() {
+        
+        let ToDoItem = this.props.createToDo ? 
+            <ToDoItem 
+                heading={this.props.heading}
+                descriptions={this.props.description} 
+            /> 
+
+            : null;
         return(
+
+
             <div id="ToDoList">
                 <h1>To Do List</h1>
-                <div className="to-do-list-container"></div>
+                <div className="to-do-list-container">
+                    {ToDoItem}
+                </div>
             </div>
         )
     }
