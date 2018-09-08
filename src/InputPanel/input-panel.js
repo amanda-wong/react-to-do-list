ff51e2d2112d8182811f229efad285f29bea95a3
Amanda <missamandawong@gmail.com>
2018-09-08 00:21:24 -0700

    Pass input to parent state

--- src/InputPanel/input-panel.js
+++ src/InputPanel/input-panel.js
@@ -2,16 +2,34 @@ import React, { Component } from 'react';
 import './input-panel.css';
 
 class InputPanel extends Component {
+
+    handleClick = () => {
+        this.props.submitHandler();
+    }
+
+    handleHeadingChange = (event) => {
+        this.props.headingChange(event);
+    }
+
+    handleDescriptionChange = (event) => {
+        this.props.descriptionChange(event);
+    }
+
     render() {
         return (
             <div id="InputPanel">
-                <input type="text" className="task-heading" />
-                <input type="text" className="task-description" />
+                <input type="text" 
+                    className="task-heading" 
+                    onChange={this.handleHeadingChange} />
+                <input 
+                    type="text" 
+                    className="task-description" 
+                    onChange={this.handleDescriptionChange} />
                 <select className="priority-rank">
                     <option value="high priority">High Priority</option>
                     <option value="low priority">Low Priority</option>
                 </select>
-                    <input type="submit" />
+                <input type="submit" onClick={this.handleClick}/>
             </div>
         )
     }
