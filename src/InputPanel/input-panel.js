ffefc2b3ad8a50e61631aaa1130219a54e9a608f
Amanda <missamandawong@gmail.com>
2018-09-09 14:41:48 -0700

    Add the ability to reset fields

--- src/InputPanel/input-panel.js
+++ src/InputPanel/input-panel.js
@@ -3,63 +3,71 @@ import './input-panel.css';
 
 class InputPanel extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
             heading: '',
             description: '',
             priority: 'high'
         };
+
+        this.baseState = this.state;
     }
 
     handleSubmit = () => {
         if(this.state.heading !== '' && this.state.description !== '') {
             this.props.submitHandler({...this.state});
-            
-            this.setState({
-                heading: '',
-                description: '',
-                priority: 'high'
-            })
+            this.resetFields();
         }
     }
 
-    handleHeadingChange = (event) => {
+    handleHeadingChange = (e) => {
         this.setState({
-            heading: event.target.value
+            heading: e.target.value
         });
     }
 
-    handleDescriptionChange = (event) => {
+    handleDescriptionChange = (e) => {
         this.setState({
-            description: event.target.value
+            description: e.target.value
         });
     }
 
-    handlePrioritySelection = (event) => {
-        if(event.target.value !== this.state.priority) {  // to prevent rerender if same selection
+    handlePrioritySelection = (e) => {
+        if(e.target.value !== this.state.priority) {  // to prevent rerender if same selection
             this.setState({
-                priority: event.target.value
+                priority: e.target.value
             });
         }
     }
 
+    resetFields = () => {
+        this.setState(this.baseState);
+    }
+    
     render() {
+        
         return (
             <div id="InputPanel">
                 <input type="text" 
                     className="task-heading" 
-                    onChange={this.handleHeadingChange} />
+                    onChange={this.handleHeadingChange}
+                    value={this.state.heading} />
                 <input 
                     type="text" 
                     className="task-description" 
-                    onChange={this.handleDescriptionChange} />
-                <select className="priority-rank" onChange={this.handlePrioritySelection}>
+                    onChange={this.handleDescriptionChange} 
+                    value={this.state.description}/>
+                <select 
+                    className="priority-rank" 
+                    onChange={this.handlePrioritySelection}
+                    value={this.state.priority}>
                     <option value="high">High Priority</option>
                     <option value="low">Low Priority</option>
                 </select>
                 <input type="submit" onClick={this.handleSubmit} />
+                <button className="clear" onClick={this.resetFields}>Clear</button>
             </div>
         )
     }
