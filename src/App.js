aec4a3fd92711118e3bc6da1205a319b5153ac27
Wong <amanwong@macc02tv00eg8wl.ca.bestbuy.com>
2018-09-12 13:04:25 -0700

    Pass done list to done component and fix toDoList array sort

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import InputPanel from './InputPanel/input-panel';
 import ToDoList from './ToDoList/to-do-list';
+import DoneList from './DoneList/done-list';
 import './App.css';
 
 class App extends Component {
@@ -8,41 +9,60 @@ class App extends Component {
         super();
         this.state = {
             toDoList: [],
-            showPanel: false
+            showPanel: false,
+            doneList: []
         }
     }
 
-    submitToDoHandler = (state) => {
-        let sortedItems = this.state.toDoList.sort((a,b) => a.priority && a.heading > b.priority && b.heading).concat(state)
+    moveItemToDone = (id) => {
+        let toDoList = this.state.toDoList.map(function(item) {
+            if(item.id !== id) {
+                return item;
+            }
+        })
+        let doneList = this.state.toDoList.find(item => (item.id === id) ? item : null)
+
         this.setState({
-            toDoList: sortedItems // how to add objects into an array
-        });
+            toDoList: toDoList,
+            doneList: this.state.doneList.concat(doneList)
+        })
     }
 
-    addToDoClick = () => {
-        this.setState({showPanel:true})
+    submitToDoHandler = (state) => {
+        let sortedItems = this.state.toDoList.concat(state).sort((a,b) => a.priority > b.priority)
+        this.setState({toDoList: sortedItems});
     }
 
-    closeModalHandler = () => {
-        this.setState({showPanel: false})
-    }
-     
+    addToDoClick = () => this.setState({showPanel:true});
+
+    closeModalHandler = () => this.setState({showPanel: false});
+
     render() {
+
+        console.log("todo list: ", this.state.toDoList);
+        console.log("done list: ", this.state.doneList);
+        console.log("todo id: ", this.state.toDoList.id);
+        console.log("done id: ", this.state.doneList.id);
+
         return (
             <div className="app">
-                <div 
+                <div
                     className="add-to-do-button"
                     onClick={this.addToDoClick}>+</div>
                 {this.state.showPanel
-                    ? <InputPanel 
+                    ? <InputPanel
                         headingChange={this.headingChangeHandler}
-                        descriptionChange={this.descriptionChangeHandler} 
+                        descriptionChange={this.descriptionChangeHandler}
                         priorityChange={this.priorityHandler}
-                        submitHandler={this.submitToDoHandler} 
+                        submitHandler={this.submitToDoHandler}
                         closeModal={this.closeModalHandler}
                         />
-                    : null}    
-                <ToDoList toDoList={this.state.toDoList} />
+                    : null}
+                <ToDoList
+                    toDoList={this.state.toDoList}
+                    checkmarkChange={this.moveItemToDone}
+                />
+                <DoneList />
             </div>
         );
     }
