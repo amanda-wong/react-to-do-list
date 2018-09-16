7c8aaef6a04f8690e209329b950f8b15938be01b
Amanda <missamandawong@gmail.com>
2018-09-16 13:55:29 -0700

    Create DoneList component

---
+++ src/DoneList/done-list.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class DoneList extends Component {
+    render() {
+        return(
+            <div className="done-list">
+                <h1>Done</h1>
+                <div className="done-list-container">
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DoneList;
